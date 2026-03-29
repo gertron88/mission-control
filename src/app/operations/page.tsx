036e04932c1f0d38dd78020a614e20526056e272
Gertron88 <Gertron88@gmail.com>
2026-03-29 02:13:20 -0400

    fix: move 'use client' directive to top of operations page

--- src/app/operations/page.tsx
+++ src/app/operations/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic'
-
 "use client";
+
+export const dynamic = 'force-dynamic'
 
 import { DashboardShell } from "@/components/layout/DashboardShell";
 import { StatusCard } from "@/components/status/StatusCard";
