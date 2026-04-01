75aa5e68b14a58b98fce07f83e787d7a278842e7
Gertron88 <Gertron88@gmail.com>
2026-04-01 18:14:55 -0400

    fix: MobileSidebar import

--- src/components/layout/MobileSidebar.tsx
+++ src/components/layout/MobileSidebar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Menu } from "lucide-react";
-import { Sidebar } from "./Sidebar";
+import Sidebar from "./Sidebar";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
